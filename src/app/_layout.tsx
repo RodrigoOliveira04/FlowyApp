47f96e6d386790b4ca45e95b95ae2d3b43858728
Julia Reis <julyreis.correa@gmail.com>
2025-12-05 17:19:28 -0300

    Tab do app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Slot } from "expo-router";
+import { Slot, Tabs } from "expo-router";
 import { StyleSheet, View } from "react-native";
 import { EntriesProvider } from "../contexts/diary/EntriesContext"; // sobe uma pasta pra src
 
@@ -26,3 +26,74 @@ const styles = StyleSheet.create({
     // opcional styling de header
   },
 });
+
+//Layour das tabs e o style sheet
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+
+        tabBarStyle: tabStyles.tabBar,
+
+        tabBarLabelStyle: tabStyles.tabLabel,
+
+        tabBarActiveTintColor: "#7CB6FF",
+        tabBarInactiveTintColor: "#2D5D9F",
+      }}
+    >
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="diary"
+        options={{
+          title: "Diário",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="book" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="exercises"
+        options={{
+          title: "Exercícios",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+const tabStyles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: "#F9FBFF", // igual ao card
+    borderTopWidth: 1,
+    borderTopColor: "#BFD8FF", // tom azul suave
+    height: 65,
+    paddingBottom: 8,
+    paddingTop: 8,
+  },
+
+  tabLabel: {
+    fontSize: 12,
+    fontWeight: "600",
+    color: "#2D5D9F", // seu azul escuro principal
+  },
+
+  tabLabelFocused: {
+    color: "#7CB6FF", // azul claro do botão
+  },
+});
+
+
